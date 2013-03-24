a662c15e0bea6bbfc7ff1a066b72d11d9de0671d
Doug Livesey <biot023@gmail.com>
2013-03-24 09:56:24 +0000

    Trying to get code to match ruby tailable cursor tutorial, but still doesn't work.

--- lib/services/main_loop.rb
+++ lib/services/main_loop.rb
@@ -5,17 +5,21 @@ module Services
   class MainLoop < Struct.new( :input_collection, :output_collection, :processors )
 
     def run( infinite=true )
-      cursor = input_collection.collection.find( :message => { :$exists => true } )
-      cursor.add_option( 2 )
-      cursor.add_option( 32 )
+      cursor = Mongo::Cursor.new( input_collection.collection,
+                                  :tailable => true,
+                                  :order => { :$natural => 1 } )
+      # cursor = input_collection.collection.find( :message => { :$exists => true } )
+      # cursor.add_option( 2 )
+      # cursor.add_option( 32 )
       begin
-        if cursor.has_next?
-          doc = cursor.next
+        doc = cursor.next
+        if doc
+          next if ! doc.has_key?( "message" )
 # TODO -- delete next line
           File.open( "log/test.log", "w+" ) { |f| f << doc.inspect << " " }
           output_collection.collection.insert( _process( doc ) )
-          input_collection.collection.update( { :_id => doc["_id"] },
-                                              { :$unset => { :message => true } } )
+          input_collection.collection.update( { "_id" => doc["_id"] },
+                                              { :$unset => { "message" => true } } )
         else
 # TODO -- delete next line
           File.open( "log/test.log", "w+" ) { |f| f << "Tick. " }
